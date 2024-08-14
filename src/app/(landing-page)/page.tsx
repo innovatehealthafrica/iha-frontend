281c3d12a5874c951e4c6bf614d9d3e5709ae3fc
cremirdevio <josephajibodu@gmail.com>
2024-08-14 02:04:07 +0100

    programsa dn initiatives section fixed

--- src/app/(landing-page)/page.tsx
+++ src/app/(landing-page)/page.tsx
@@ -82,9 +82,9 @@ export default function Home() {
 
       {/* Programs and Initiatives Section */}
       <section className="bg-primary-green/85 bg-no-repeat bg-cover">
-        <div className="flex flex-col lg:flex-row py-28 px-24 space-x-12 max-w-screen-2xl mx-auto">
+        <div className="flex flex-col lg:flex-row py-28 px-8 lg:px-24 space-x-12 max-w-screen-2xl mx-auto">
           <div className="flex flex-col justify-center w-full">
-            <h2 className="font-[500] text-5xl text-white">
+            <h2 className="font-[500] text-4xl lg:text-5xl text-white">
               Our Programs & Initiatives
             </h2>
 
