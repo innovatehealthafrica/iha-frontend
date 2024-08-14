a305ef29decbdaca6c72a4ee4dd264ab73d725a3
cremirdevio <josephajibodu@gmail.com>
2024-08-14 02:14:25 +0100

    connect with innovators made responsive

--- src/app/(landing-page)/components/connect-with-innovators.tsx
+++ src/app/(landing-page)/components/connect-with-innovators.tsx
@@ -8,25 +8,28 @@ import toast from "react-hot-toast";
 
 export default function ConnectWithInnovatorSection() {
   return (
-    <section className="">
-      <div className="flex gap-16">
+    <section className="border-t md:border-none">
+      <div className="flex flex-col lg:flex-row gap-16">
         <Image
           src={mapImage}
           alt="Connect with innovators"
-          className="w-full"
+          className="w-full hidden lg:block"
         />
-        <div className="flex flex-col justify-center w-full">
-          <h1 className="font-[600] text-4xl leading-normal max-w-sm">
+        <div className="flex flex-col justify-center w-full px-8 lg:px-0 py-28 lg:py-0">
+          <h1 className="font-[600] text-3xl lg:text-4xl lg:leading-normal max-w-sm">
             Connect With Innovators Across Africa
           </h1>
 
-          <p className="mt-12 max-w-screen-sm">
+          <p className="mt-6 lg:mt-12 max-w-screen-sm">
             Search our vibrant network of over 2,000 healthcare innovators,
             fostering collaboration and growth across the continent.
           </p>
 
-          <div className="w-full max-w-60 relative mt-6">
-            <Input placeholder="Country" className="w-full rounded-full h-12" />
+          <div className="w-full md:max-w-60 relative mt-6">
+            <Input
+              placeholder="Country"
+              className="w-full rounded-full h-12 text-lg px-8"
+            />
             <Button
               onClick={() => {
                 toast.error("Feature coming soon.", {
