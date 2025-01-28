55bbf483bf295cf237ba42d8dec3dfba439a78d0
Joseph Ajibodu <josephajibodu@gmail.com>
2025-01-28 22:42:00 +0100

    our work section completed

--- src/app/(landing-page)/page.tsx
+++ src/app/(landing-page)/page.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import heroSlide1 from "@/assets/images/hero/hero_slide_1.png";
 import heroSlide2 from "@/assets/images/hero/hero_slide_2.png";
 import heroSlide3 from "@/assets/images/hero/hero_slide_3.png";
+import background from "@/assets/images/background.png";
 import KeySolutionsSection from "./components/key-solutions-section";
 import TestimonialSection from "./components/testimonial-section";
 import ConnectWithInnovatorSection from "./components/connect-with-innovators";
@@ -20,6 +21,7 @@ import {
 } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
 import Fade from "embla-carousel-fade";
+import OurWorkSection from "./components/our-work-section";
 
 const statistics = [
   { label: "Healthcare solution developed", value: 250 },
@@ -32,18 +34,21 @@ export default function Home() {
     <>
       {/* Hero Section */}
       <section className="bg-primary-green/85 bg-no-repeat bg-cover relative">
-        <div className="absolute inset-0 flex flex-col items-center justify-center max-w-screen-2xl mx-auto z-10">
-          <h1 className="text-white font-bold text-center text-5xl lg:text-6xl lg:leading-tight">
-            Advancing <span className="text-accent">Equitable Healthcare</span>{" "}
-            across <br />
-            Africa through Digital Innovation
-          </h1>
-          <p className="py-2.5 px-8 mt-9 rounded-lg text-2xl font-[500] bg-primary/25 backdrop-blur text-white border border-white/25">
-            By Africans, for Africa
-          </p>
-          <Button className="mt-14 text-md" size="lg" variant="secondary">
-            Learn More
-          </Button>
+        <div className="absolute inset-0 z-10">
+          <div className="flex flex-col items-center justify-center max-w-screen-xl mx-auto h-full">
+            <h1 className="text-white font-bold text-center text-5xl lg:text-6xl lg:leading-tight">
+              Advancing{" "}
+              <span className="text-accent">Equitable Healthcare</span> across{" "}
+              <br />
+              Africa through Digital Innovation
+            </h1>
+            <p className="py-2.5 px-8 mt-9 rounded-lg text-2xl font-[500] bg-primary/25 backdrop-blur text-white border border-white/25">
+              By Africans, for Africa
+            </p>
+            <Button className="mt-14 text-md" size="lg" variant="secondary">
+              Learn More
+            </Button>
+          </div>
         </div>
 
         <Carousel
@@ -90,7 +95,7 @@ export default function Home() {
       </section>
 
       {/* Statistics */}
-      <section className="-mt-[25%] sm:-mt-[12%] lg:-mt-[5%] lg:absolute w-full">
+      <section className="-mt-[25%] sm:-mt-[12%] lg:-mt-[5%] lg:absolute w-full z-10">
         <div className="shadow-lg rounded-3xl grid grid-cols-1 lg:grid-cols-3 py-8 px-8 lg:px-24 gap-4 sm:gap-6 lg:gap-8 max-w-screen-xl mx-auto bg-white">
           {statistics.map((stat, index) => (
             <div key={index} className="col-span-1 px-4 space-y-2 sm:space-y-3">
@@ -105,32 +110,46 @@ export default function Home() {
         </div>
       </section>
 
-      {/* Key Solutions */}
-      <KeySolutionsSection />
+      <section
+        className="relative"
+        style={{
+          backgroundImage: `url(${background.src})`,
+          backgroundSize: "contain",
+          backgroundPosition: "center",
+        }}
+      >
+        {/* White overlay */}
+        <div className="absolute inset-0 bg-white opacity-85"></div>
 
-      {/* Programs and Initiatives Section */}
-      <section className="bg-primary-green/85 bg-no-repeat bg-cover">
-        <div className="flex flex-col lg:flex-row py-28 px-8 lg:px-24 space-x-12 max-w-screen-2xl mx-auto">
-          <div className="flex flex-col justify-center w-full">
-            <h2 className="font-[500] text-4xl lg:text-5xl text-white">
-              Our Programs & Initiatives
-            </h2>
+        <div className="relative z-10">
+          {/* Our Work Section */}
+          <OurWorkSection />
 
-            <p className="mt-12 text-white">
-              Our programs and initiatives empower Africa healthcare innovators
-              with the support, skills and resources to drive innovation,
-              transform ideas into impactful products, and improve healthcare
-              outcome—positively impacting millions of lives across the
-              continent
-            </p>
-          </div>
+          {/* Programs and Initiatives Section */}
+          <section className="bg-primary-green/85 bg-no-repeat bg-cover">
+            <div className="flex flex-col lg:flex-row py-28 px-8 lg:px-24 space-x-12 max-w-screen-2xl mx-auto">
+              <div className="flex flex-col justify-center w-full">
+                <h2 className="font-[500] text-4xl lg:text-5xl text-white">
+                  Our Programs & Initiatives
+                </h2>
 
-          <div className=""></div>
+                <p className="mt-12 text-white">
+                  Our programs and initiatives empower Africa healthcare
+                  innovators with the support, skills and resources to drive
+                  innovation, transform ideas into impactful products, and
+                  improve healthcare outcome—positively impacting millions of
+                  lives across the continent
+                </p>
+              </div>
+
+              <div className=""></div>
+            </div>
+          </section>
+
+          {/* Testimonials */}
+          <TestimonialSection />
         </div>
       </section>
-
-      {/* Testimonials */}
-      <TestimonialSection />
 
       {/* Connect With Innovators */}
       <ConnectWithInnovatorSection />
